e342c643aa3d5ccb910ec342bcd1f8fd1cbfd390
franciscocpereira <fmcp31@gmail.com>
2021-11-25 18:14:34 +0000

    3rd commit

--- js/termynal.js
+++ js/termynal.js
@@ -1,8 +1,10 @@
-var termynal = new Termynal('#termynal', {
-    lineData: [
-        { type: 'input', value: 'python' },
-        { value: 'Are you sure you want to install \'spaCy\'?' },
-        { type: 'input',  typeDelay: 1000, prompt: '(y/n)', value: 'y' },
-        { delay: 1000, value: 'Installing spaCy...' }
-    ]
-});
+var termynal = new Termynal('#termynal',
+    {
+        lineData: [
+            { value: 'python' },
+            { value: '>>> import meight' },
+            { type: 'input',  typeDelay: 1000, prompt: '(y/n)', value: 'y' },
+            { delay: 1000, value: 'Installing spaCy...' }
+        ]
+    }
+)
